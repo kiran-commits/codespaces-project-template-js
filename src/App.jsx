eb8d70b9b9e50435c2b509734705889aece4dd56
kiran-commits <ithapetanu@gmail.com>
2024-07-03 05:17:51 +0000

    image added

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Kiran Ithape",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  email: "ithapek2@gmail.com.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "kiranithae",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/work-desk";
+import image from "../images/work-desk.jpg";
 
 const imageAltText = "desktop and laptop";
 
